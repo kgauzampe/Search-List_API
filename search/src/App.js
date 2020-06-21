44fbe70c451ed49f0261b10bbce345253693663a
kgauzampe <kgauza.mpe@gmail.com>
2020-06-21 17:05:37 +0200

    files added

---
+++ search/src/App.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+
+
+class  App extends Component() {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            Movies: [],
+            loaded: false,
+        }
+    }
+
+    componentDidMount() {
+        fetch('http://www.omdbapi.com/?i=tt3896198&apikey=1302d05c')
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    loaded: true,
+                    Movies: json, 
+                })
+
+            });
+    }
+
+    render() {
+
+        let { loaded, Movies} = this.state
+
+        if (!loaded) {
+            return <div>Loading...</div>
+        } else { 
+            return (
+                <div className="App">
+                    Movies loaded!!
+                </div>
+            )
+        }
+       
+    }
+  
+}
+export default App;
